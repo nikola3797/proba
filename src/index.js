c75b819549d764f9b4cb3fccff82d53f74f55e67
Nikola Veljic <nikola.zeka97@gmail.com>
2020-11-09 14:55:23 +0100

    Sada sam nesto izmenio

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
+//ovo je najeveca kurcina koja je se ikada dogodila koj kurac se desava
 import './index.css';
 import App from './App';
 
