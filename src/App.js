705af0f4be9c9e747a83a89abf639d0f906bef3f
Nikola Veljic <nikola.zeka97@gmail.com>
2020-11-09 14:53:15 +0100

    Drugi commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 import Ingredients from './components/Ingredients/Ingredients';
-
+//nije ovo moja greska pusite kurac i da popusite picketinu malu smrdljuvy
 const App = props => {
   return <Ingredients />;
 };
